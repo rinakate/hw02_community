2efc308da1b9769923f99eebc463daa9c1f09d80
Ekaterina Tishchenko <Ekaterina.Tishchenko@yandex.ru>
2021-03-01 21:32:44 +0300

    test success

---
+++ posts/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import get_object_or_404, render
+
+from .models import Group, Post
+
+
+def index(request):
+    latest = Post.objects.order_by("-pub_date")[:11]
+    return render(request, "index.html", {"posts": latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    return render(request, "group.html", {"group": group, "posts": posts})
